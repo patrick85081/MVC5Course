cdca8ead8cd2f6d436c2813a4077964270baae6d
Patrick Shih <patrick85081@gmail.com>
2018-07-08 10:44:11 +0800

    finish ClientController

--- MVC5Course/Models/Client.Partial.cs
+++ MVC5Course/Models/Client.Partial.cs
@@ -1,14 +1,35 @@
+using MVC5Course.Models.Validation;
+
 namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    
+
     [MetadataType(typeof(ClientMetaData))]
-    public partial class Client
+    public partial class Client : IValidatableObject
     {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (this.DateOfBirth == null)
+                yield return new ValidationResult("出生格式錯誤", new[] {nameof(DateOfBirth)});
+            else if (
+                this.DateOfBirth.Value.Year > 1980 && this.City == "Taipei")
+            {
+                yield return new ValidationResult(
+                    "台北出身的一定為1980年後出生",
+                    new string[] {"DateOfBirth", "City"});
+            }
+
+            if (this.Latitude.HasValue != this.Longitude.HasValue)
+            {
+                yield return new ValidationResult(
+                    "經緯度欄位必須一起有值",
+                    new[] {nameof(Longitude), nameof(Latitude)});
+            }
+        }
     }
-    
+
     public partial class ClientMetaData
     {
         [Required]
@@ -25,12 +46,16 @@ namespace MVC5Course.Models
         
         [StringLength(1, ErrorMessage="欄位長度不得大於 1 個字元")]
         public string Gender { get; set; }
-        public Nullable<System.DateTime> DateOfBirth { get; set; }
-        public Nullable<double> CreditRating { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+
+        public double? CreditRating { get; set; }
         
         [StringLength(7, ErrorMessage="欄位長度不得大於 7 個字元")]
         public string XCode { get; set; }
-        public Nullable<int> OccupationId { get; set; }
+        public int? OccupationId { get; set; }
         
         [StringLength(20, ErrorMessage="欄位長度不得大於 20 個字元")]
         public string TelephoneNumber { get; set; }
@@ -46,10 +71,16 @@ namespace MVC5Course.Models
         
         [StringLength(15, ErrorMessage="欄位長度不得大於 15 個字元")]
         public string ZipCode { get; set; }
-        public Nullable<double> Longitude { get; set; }
-        public Nullable<double> Latitude { get; set; }
+
+        public double? Longitude { get; set; }
+        public double? Latitude { get; set; }
+
         public string Notes { get; set; }
-    
+
+
+        [身份證字號驗證]
+        public string IdNumber { get; set; }
+
         public virtual Occupation Occupation { get; set; }
         public virtual ICollection<Order> Order { get; set; }
     }
---
+++ MVC5Course/Models/Validation/身份證字號驗證Attribute.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models.Validation
+{
+    public class 身份證字號驗證Attribute : DataTypeAttribute
+    {
+        public 身份證字號驗證Attribute() : base(DataType.Text)
+        {
+            ErrorMessage = "請輸入正確的身份證字號";
+        }
+
+        public override bool IsValid(object value)
+        {
+            string str = (string) value;
+
+            return IsIdentificationId(str);
+        }
+
+        public bool IsIdentificationId(string identify)
+        {
+            if (string.IsNullOrEmpty(identify))
+                return true;
+
+            var d = false;
+            if (identify.Length == 10)
+            {
+                identify = identify.ToUpper();
+                if (identify[0] >= 0x41 && identify[0] <= 0x5A)
+                {
+                    var a = new[]
+                    {
+                        10, 11, 12, 13, 14, 15, 16, 17, 34, 18, 19, 20, 21, 22, 35, 23, 24, 25, 26, 27, 28, 29, 32, 30,
+                        31, 33
+                    };
+                    var b = new int[11];
+                    b[1] = a[(identify[0]) - 65] % 10;
+                    var c = b[0] = a[(identify[0]) - 65] / 10;
+                    for (var i = 1; i <= 9; i++)
+                    {
+                        b[i + 1] = identify[i] - 48;
+                        c += b[i] * (10 - i);
+                    }
+
+                    if (((c % 10) + b[10]) % 10 == 0)
+                    {
+                        d = true;
+                    }
+                }
+            }
+
+            return d;
+        }
+    }
+}
