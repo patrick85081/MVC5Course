ddcb62f2aa736810ec62404a33eb99a8345db797
Patrick Shih <patrick85081@gmail.com>
2018-07-14 10:55:02 +0800

    ViewBag & ViewData & TempData Demo

---
+++ MVC5Course/Controllers/ModelBindingController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class ModelBindingController : Controller
+    {
+        // GET: ModelBinding
+        public ActionResult Index()
+        {
+            var data = "Hello World";
+            ViewData.Model = data;
+            return View();
+        }
+
+        public ActionResult ViewBagDemo()
+        {
+            ViewBag.Text = "Demo";
+            return View();
+        }
+
+        public ActionResult ViewDataDemo()
+        {
+            ViewData["Text"] = "Demo";
+            return View();
+        }
+
+        public ActionResult TempDataSave()
+        {
+            TempData["Text"] = "Temp";
+            return RedirectToAction("TempDataDemo");
+        }
+
+        public ActionResult TempDataDemo()
+        {
+            return View();
+        }
+    }
+}
